4822cb0bb9f5c5566c5f3c2d0db1c98d313c7afe
Ambika <ambika.balasubramanian@accoliteindia.com>
2017-07-11 09:00:13 +0530

    Done

---
+++ src/myPack/myClass.java
@@ -0,0 +1,24 @@
+package myPack;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class myClass.
+ */
+public class myClass {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+		int j = 0;
+
+		for (j = 0; j < 7; j++) {
+			System.out.println(j);
+
+		}
+	}
+}
